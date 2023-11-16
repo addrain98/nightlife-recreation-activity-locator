d0f7766bf52297cf98ad1a2768f9e772a69326d1
Jia Yu Koh <kohjiayu1998@gmail.com>
2023-11-16 10:01:42 +0000

    added a search bar and added the markers for all nightclub location

---
+++ map.js
@@ -0,0 +1,14 @@
+function initMap() {
+    const singapore = [1.3521, 103.8198]
+    const map = L.map("map");
+    map.setView(singapore, 13)
+
+    L.tileLayer('https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+        attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        minZoom: 0,
+        maxZoom: 22,
+        subdomains: 'abcd',
+        accessToken: 'kpWYzvRLkzuDJkOjWV34GNVHiyA9wKZOgt8r5LzmMIVfgqFGVsPJnjJTjKqrvEGV'
+    }).addTo(mapObject);
+    return map;
+}
