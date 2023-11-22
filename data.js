478f74435c99963fd59b1261779fe0a7efb0958e
Jia Yu Koh <kohjiayu1998@gmail.com>
2023-11-22 10:09:54 +0000

    added search result and added div to result item

--- data.js
+++ data.js
@@ -1,16 +1,29 @@
-async function find(searchTerms, lat, lng) {
+API_KEY = 'fsq3oup5rBr57qce1vJYCdwbnp2lm1aZ6kWu4DL93TyuG+E='
+async function find(searchTerms, lat, lng, radius = 10000) {
 
     const response = await axios.get("https://api.foursquare.com/v3/places/search",{
        // check the FourSquare documentation (the Places API)
         params: {
             query: searchTerms, 
             ll: lat+","+lng, 
-            limit: 50
+            limit: 50,
+            radius: radius
         },
         headers: {
             accept: 'application/json',
-            Authorization: 'fsq3oup5rBr57qce1vJYCdwbnp2lm1aZ6kWu4DL93TyuG+E='
+            Authorization: API_KEY
           }
     });
+    return response.data;
+}
+
+async function loadPhoto(fsq_id) {
+    const response = await axios.get(`https://api.foursquare.com/v3/places/${fsq_id}/photos`, {
+        headers: {
+            accept: 'application/json',
+            Authorization: API_KEY
+          }
+    });
+
     return response.data;
 }
--- frontend.js
+++ frontend.js
@@ -20,26 +20,57 @@ document.addEventListener("DOMContentLoaded", async function(){
    
 
     function displaySearchResults(results) {
-        
         for (let r of results.results) {
-            const lat = r.geocodes.main.latitude;
+            const marker = addMarkerToMap(map,r);
+    
+                const searchResultDiv = document.querySelector("#search-results");
+                const resultElement = document.createElement(`div`);
+                resultElement.innerHTML = r.name;
+                searchResultDiv.appendChild(resultElement);
+                resultElement.classList.add("result-item");
+                resultElement.addEventListener("click", function(){
+                    map.flyTo(coordinate, 20);
+                    marker.openPopup();
+                });
+
+            
+        }
+
+    }
+
+    function addMarkerToMap(map,r){
+        const lat = r.geocodes.main.latitude;
             const lng = r.geocodes.main.longitude;
             const coordinate = [lat, lng];
             const marker = L.marker(coordinate);
-            const isUnderNightClubCategory = r.categories.some(category => category.id === 10032);
-            if (isUnderNightClubCategory && isInSG(lat, lng)) {
-                marker.addTo(searchResultLayer);
-            }
-            marker.bindPopup(`<h1>${r.name}<\h1>`)
-            marker.addEventListener('click', function(){
-                map.flyTo(coordinate, 16);
-            })
+    
+    
+        if (isNightClubCategory(r.categories) && isInSG(lat, lng)) {
+            marker.addTo(searchResultLayer);
         }
-    } 
+        marker.bindPopup(function(){
+            const element = document.createElement('div');
+            element.innerHTML = `<h1>${r.name}</h1>`
+        });
 
+        marker.addEventListener('click', function(){
+            map.flyTo(coordinate, 16);
+        });
+
+        return marker;    
+    }
+
+    function isNightClubCategory(categories) {
+        for (let i = 0; i < categories.length; i++) {
+            if (categories[i].id === 10032) {
+                return true; 
+            }
+        }
+        return false; 
+    }
     function isInSG(lat, lng) {
         const latMin = 1.22;
-        const latMax = 1.47;
+        const latMax = 1.45;
         const lngMin = 103.6;
         const lngMax = 104.0;
     
