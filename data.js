b04e0cdae2cb14e83cbabbb2da4b970c234f593b
Jia Yu Koh <kohjiayu1998@gmail.com>
2023-12-05 16:58:04 +0000

    Added supper place, allow axios.get the right API key based on searchTerms. Have not add new layer for supper yet

--- data.js
+++ data.js
@@ -1,5 +1,18 @@
-API_KEY = 'fsq3oup5rBr57qce1vJYCdwbnp2lm1aZ6kWu4DL93TyuG+E='
+const API_KEY = {
+    'fsq3oup5rBr57qce1vJYCdwbnp2lm1aZ6kWu4DL93TyuG+E=':['nightclub', 'club'],
+    'fsq3kiOZiK5hbZtwsuW3JhMb0Pjgh6UrHtN1Dt9yu+PLD1o=':['supper', 'restuarant', 'food']
+}
+function getApiKeyFromSearchTerm(searchTerms) {
+    for (const [apiKey, terms] of Object.entries(API_KEY)){
+        if (terms.includes(searchTerms)){
+            return apiKey
+        }
+    }
+
+}
 async function find(searchTerms, lat, lng, radius = 10000) {
+
+    const apiKey = getApiKeyFromSearchTerm(searchTerms)
 
     const response = await axios.get("https://api.foursquare.com/v3/places/search",{
        // check the FourSquare documentation (the Places API)
@@ -11,13 +24,13 @@ async function find(searchTerms, lat, lng, radius = 10000) {
         },
         headers: {
             accept: 'application/json',
-            Authorization: API_KEY
+            Authorization: apiKey
           }
     });
     return response.data;
 }
 
-async function loadPhoto(fsq_id) {
+/*async function loadPhoto(fsq_id) {
     const response = await axios.get(`https://api.foursquare.com/v3/places/${fsq_id}/photos`, {
         headers: {
             accept: 'application/json',
@@ -26,4 +39,4 @@ async function loadPhoto(fsq_id) {
     });
 
     return response.data;
-}
+}*/
