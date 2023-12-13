47c4fbb1c320439833a87c5d4316b8e3c22f3195
Jia Yu Koh <kohjiayu1998@gmail.com>
2023-12-13 13:33:32 +0000

    random edits

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 function initMap() {
-    const singapore = [1.3521, 103.8198]
+    const singapore = [1.35, 103.81]
     const map = L.map("map");
-    map.setView(singapore, 13)
+    map.setView(singapore, 12)
 
     L.tileLayer('https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
         attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
