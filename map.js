a2777d505a91759d5f71828ee60402642e86a6e3
Jia Yu Koh <kohjiayu1998@gmail.com>
2024-01-10 13:21:25 +0000

    removed additional zoom icon

--- map.js
+++ map.js
@@ -1,18 +1,22 @@
 function initMap() {
     const singapore = [1.35, 103.81]
-    const map = L.map("map");
+    const map = L.map("map", {
+        zoomControl : false
+    });
+    
     map.setView(singapore, 12)
 
     L.control.zoom({
         position: "bottomright"
     }).addTo(map);
-    
+
     L.tileLayer('https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
         attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         minZoom: 0,
         maxZoom: 22,
         subdomains: 'abcd',
-        accessToken: 'kpWYzvRLkzuDJkOjWV34GNVHiyA9wKZOgt8r5LzmMIVfgqFGVsPJnjJTjKqrvEGV'
+        accessToken: 'kpWYzvRLkzuDJkOjWV34GNVHiyA9wKZOgt8r5LzmMIVfgqFGVsPJnjJTjKqrvEGV',
+        position: "bottomright"
     }).addTo(map);
     
     return map;
