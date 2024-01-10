2b02a8cd6df2c88277eb0438146d30a17ab464fd
Jia Yu Koh <kohjiayu1998@gmail.com>
2024-01-10 13:12:41 +0000

    shift zoom icon to bottom right

--- map.js
+++ map.js
@@ -3,6 +3,10 @@ function initMap() {
     const map = L.map("map");
     map.setView(singapore, 12)
 
+    L.control.zoom({
+        position: "bottomright"
+    }).addTo(map);
+    
     L.tileLayer('https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
         attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         minZoom: 0,
